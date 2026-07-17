2111d3d0f094060a3bcb0610df5503f0e9c062b4
joslfer <jose.luefer@gmail.com>
2026-07-17 14:38:29 +0200

    neighbor interactions

--- src/games/prisoners_dilemma.py
+++ src/games/prisoners_dilemma.py
@@ -1,5 +1,8 @@
 # Prisoner's Dilemma payoff function
 
+import numpy as np 
+
+
 T = 5  # Temptation: payoff for defecting while the other cooperates
 R = 3  # Reward: payoff when both cooperate
 P = 1  # Punishment: payoff when both defect
@@ -17,22 +20,25 @@ def play_round(cooperates_a, cooperates_b):
     return payoffs[(cooperates_a, cooperates_b)]
 
 
-def total_payoffs_network(population):
+def total_payoffs_network(population,G):
     payoffs = np.zeros(len(population))
 
     for i, strategy_i in enumerate(population):
         for j in list(G.neighbors(i)):
-            if i == j:
-                continue
+            #if i == j:
+                #continue
             payoff_i, payoff_j = play_round(population[i],population[j])
             payoffs[i] += payoff_i
             # print(f"Agent {i} vs Agent {j}: {population[i]} vs {population[j]} -> payoff_i={payoff_i}, accumulated={payoffs[i]}")
     return np.array(payoffs)
 
 def new_population(population,payoffs,G):
-
-
-    ###
-
-    new_pop = population    
+    new_pop = np.zeros(len(population)).astype(bool)
+    for i, strategy_i in enumerate(population):
+        paycompare = payoffs[i]
+        new_pop[i] = strategy_i
+        for neighbour in list(G.neighbors(i)):
+            if payoffs[neighbour] > paycompare:
+                new_pop[i] = population[neighbour] 
+                paycompare = payoffs[neighbour]
     return new_pop
